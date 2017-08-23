ff5929e3cebec09bb33cbf2c233a0aef0f69ab7f
Katarina Petersen <8555@RTS-365.dk>
2017-08-23 17:44:34 +0200

    aflevering

--- lokker1.js
+++ lokker1.js
@@ -1,6 +1,32 @@
+// ovelse 1
 var increase = 1;
 
 do{
     console.log (increase)
     increase++;
 }while(increase < 10)
+
+// ovelse 2
+var talOp = 1;
+
+while (talOp < 25){
+    console.log (talOp + "while");
+    talOp++;
+}
+
+do{
+    console.log (talOp + "do");
+    talOp++;
+}while (talOp < 25);
+
+for(talOp = 1; talOp < 25; talOp++){
+    console.log (talOp + "for");
+}
+
+// ovelse 3
+
+var year = 2017;
+
+for(year = 2017; year >= 1917; year--){
+    console.log (year);
+}
