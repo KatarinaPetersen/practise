eb76cb2195d6e4a19c4688789b09c295a6749d34
KatarinaPetersen <8555@rts-365.dk>
2017-08-24 13:20:07 +0200

    new

--- funktioner/hoisting.js
+++ funktioner/hoisting.js
@@ -1,51 +1,51 @@
-// ovelse 1
-// A. Udskriv profil
-function mee(navn, efternavn, dato) { //funktionen mee indeholder ARGUMENTERNE navn, efternavn og dato
-    console.log(navn + " " + efternavn + " " + dato); // VÆRDIERNE af ARGUMENTERNE bliver udskrevet i terminalen
-}
-mee('Katarina', 'Petersen', '13. december 1979'); //Der foretages FUNKTIONSKALD og VÆRDIERNE til ARGUMENTERNE bliver overført
+// // ovelse 1
+// // A. Udskriv profil
+// function mee(navn, efternavn, dato) { //funktionen mee indeholder ARGUMENTERNE navn, efternavn og dato
+//     console.log(navn + " " + efternavn + " " + dato); // VÆRDIERNE af ARGUMENTERNE bliver udskrevet i terminalen
+// }
+// mee('Katarina', 'Petersen', '13. december 1979'); //Der foretages FUNKTIONSKALD og VÆRDIERNE til ARGUMENTERNE bliver overført
 
-// B. Regnefunktioner
+// // B. Regnefunktioner
 
-function plus(tal1, tal2) {
-    console.log (tal1 + tal2);
-}
-function minus(tal1, tal2) {
-    console.log (tal1 - tal2);
-}
-function gange(tal1, tal2) {
-    console.log (tal1 * tal2);
-}
-function divider(tal1, tal2) {
-    console.log (tal1 / tal2);
-}
+// function plus(tal1, tal2) {
+//     console.log (tal1 + tal2);
+// }
+// function minus(tal1, tal2) {
+//     console.log (tal1 - tal2);
+// }
+// function gange(tal1, tal2) {
+//     console.log (tal1 * tal2);
+// }
+// function divider(tal1, tal2) {
+//     console.log (tal1 / tal2);
+// }
 
-plus(2, 4);
-minus(2, 4);
-gange(2, 4);
-divider(2, 4);
+// plus(2, 4);
+// minus(2, 4);
+// gange(2, 4);
+// divider(2, 4);
 
 
 
 var result = 0;
 
-function add(num){
+function plus(num){
     result += num;
+    console.log(result);
 }
-add(10);
-
-function sub(num){
+function minus(num){
     result -= num;
+    console.log(result);
 }
-sub(5);
-
-function mul(num){
+function gange(num){
     result *= num;
+    console.log(result);
 }
-mul(4);
-
-function div(num){
+function divider(num){
     result /= num;
+    console.log(result);
 }
-div(5);
-
+plus(20);
+minus(16);
+gange(2);
+divider(3);
