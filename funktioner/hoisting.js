7e01909b21e7f4f40fca4bb3e046a6721a970934
KatarinaPetersen <8555@rts-365.dk>
2017-08-24 07:59:53 +0200

    aflevering

---
+++ funktioner/hoisting.js
@@ -0,0 +1,27 @@
+// ovelse 1
+// A. Udskriv profil
+function mee(navn, efternavn, dato) { //funktionen mee indeholder ARGUMENTERNE navn, efternavn og dato
+    console.log(navn + " " + efternavn + " " + dato); // VÆRDIERNE af ARGUMENTERNE bliver udskrevet i terminalen
+}
+mee('Katarina', 'Petersen', '13. december 1979'); //Der foretages FUNKTIONSKALD og VÆRDIERNE til ARGUMENTERNE bliver overført
+
+// B. Regnefunktioner
+
+function plus(tal1, tal2) {
+    console.log (tal1 + tal2);
+}
+function minus(tal1, tal2) {
+    console.log (tal1 - tal2);
+}
+function gange(tal1, tal2) {
+    console.log (tal1 * tal2);
+}
+function divider(tal1, tal2) {
+    console.log (tal1 / tal2);
+}
+
+plus(2, 4);
+minus(2, 4);
+gange(2, 4);
+divider(2, 4);
+
